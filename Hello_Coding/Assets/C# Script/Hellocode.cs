c9f8ff72f9f049f678349e325df4619a5016ac47
O-S-H <dhtjdgus153@naver.com>
2022-01-14 12:28:13 +0900

    HelloWorld!(2)
    20220114(2)

--- Hello_Coding/Assets/C# Script/Hellocode.cs
+++ Hello_Coding/Assets/C# Script/Hellocode.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Hellocode : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
+
         Debug.Log("Hello World!");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+} 
