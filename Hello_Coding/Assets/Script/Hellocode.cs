910e11e3f149342aeca84479e84854fb18aadc45
O-S-H <dhtjdgus153@naver.com>
2022-01-17 12:39:20 +0900

    save  1.17
    save  1.17

--- Hello_Coding/Assets/Script/Hellocode.cs
+++ Hello_Coding/Assets/Script/Hellocode.cs
@@ -8,8 +8,19 @@ public class Hellocode : MonoBehaviour
 
     void Start()
     {
+        //캐릭터의 프로필을 변수로 만들기
+        string characterName = "라라";
+        char bloodType = 'A';
+        int age = 17;
+        float height = 163.8f;
+        bool isFemale = true;
 
-        Debug.Log("Hello World1!");
+        //생성한 변수를 콘솔에 출력
+        Debug.Log("캐릭터 이름 : " + characterName);
+        Debug.Log("혈액형 :" + bloodType);
+        Debug.Log("나이 :" + age);
+        Debug.Log("키 :" + height);
+        Debug.Log(" 여성인가?:" + isFemale);
     }
 
 } 
