3f87bfa223153d0946259386810c7868b62edd3d
O-S-H <dhtjdgus153@naver.com>
2022-01-18 12:41:35 +0900

    save p.173
    save p.173

--- Hello_Coding/Assets/Script/Hellocode.cs
+++ Hello_Coding/Assets/Script/Hellocode.cs
@@ -8,19 +8,20 @@ public class Hellocode : MonoBehaviour
 
     void Start()
     {
-        //캐릭터의 프로필을 변수로 만들기
-        string characterName = "라라";
-        char bloodType = 'A';
-        int age = 17;
-        float height = 163.8f;
-        bool isFemale = true;
 
-        //생성한 변수를 콘솔에 출력
-        Debug.Log("캐릭터 이름 : " + characterName);
-        Debug.Log("혈액형 :" + bloodType);
-        Debug.Log("나이 :" + age);
-        Debug.Log("키 :" + height);
-        Debug.Log(" 여성인가?:" + isFemale);
+        int[] students = new int[5];
+
+        students[0] = 100;
+        students[1] = 90;
+        students[2] = 80;
+        students[3] = 70;
+        students[4] = 60;
+
+        Debug.Log("0번학생의 점수" + students[0]);
+        Debug.Log("1번학생의 점수" + students[1]);
+        Debug.Log("2번학생의 점수" + students[2]);
+        Debug.Log("3번학생의 점수" + students[3]);
+        Debug.Log("4번학생의 점수" + students[4]);
     }
 
 } 
