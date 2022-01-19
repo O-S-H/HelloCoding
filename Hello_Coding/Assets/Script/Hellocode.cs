f9d0d2d99af8fa7678e1cf1991b25e33b0dc329a
O-S-H <dhtjdgus153@naver.com>
2022-01-19 11:47:21 +0900

    save
    1.19 save

--- Hello_Coding/Assets/Script/Hellocode.cs
+++ Hello_Coding/Assets/Script/Hellocode.cs
@@ -9,7 +9,22 @@ public class Hellocode : MonoBehaviour
     void Start()
     {
 
-        int[] students = new int[5];
+        //int[] students = new int[5];
+
+        //students[0] = 100;
+        // students[1] = 90;
+        // students[2] = 80;
+        // students[3] = 70;
+        // students[4] = 60;
+
+        // Debug.Log("0번학생의 점수" + students[0]);
+        // Debug.Log("1번학생의 점수" + students[1]);
+        // Debug.Log("2번학생의 점수" + students[2]);
+        //Debug.Log("3번학생의 점수" + students[3]);
+        // Debug.Log("4번학생의 점수" + students[4]);
+
+
+        int[] students = new int [5];
 
         students[0] = 100;
         students[1] = 90;
@@ -17,11 +32,14 @@ public class Hellocode : MonoBehaviour
         students[3] = 70;
         students[4] = 60;
 
-        Debug.Log("0번학생의 점수" + students[0]);
-        Debug.Log("1번학생의 점수" + students[1]);
-        Debug.Log("2번학생의 점수" + students[2]);
-        Debug.Log("3번학생의 점수" + students[3]);
-        Debug.Log("4번학생의 점수" + students[4]);
+        for (int i = 4; i >= 0; i--)
+        {
+
+            Debug.Log((i + 1) +"번 학생의 점수 "+ students[i] );
+            
+        }
+
+
     }
 
 } 
